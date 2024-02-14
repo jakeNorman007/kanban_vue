4a1900763af608a665336c047d60b35352fd87cf
jakeNorman007 <jacob.a.norman91@gmail.com>
2024-02-14 07:52:28 -0500

    .

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 import { VueQueryPlugin } from "@tanstack/vue-query";
+import router from "./routes";
 
 const app = createApp(App);
 
+app.use(router);
 app.use(VueQueryPlugin);
-app.mount('#app');
+app.mount("#app");
