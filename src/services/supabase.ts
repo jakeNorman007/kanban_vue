cd6edd6c13e1b97a30c3f3f273bafa4d094454e7
jakeNorman007 <jacob.a.norman91@gmail.com>
2024-02-14 06:36:14 -0500

    added types in supabase

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
+import { Database } from "../types/schema";
 
 export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
 
 export default supabase;
