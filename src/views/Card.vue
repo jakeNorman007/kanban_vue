84d09b7ee1c631a88cf74ae41d6004e4e27d3567
jakeNorman007 <jacob.a.norman91@gmail.com>
2024-02-24 20:59:56 -0500

    ..

--- src/views/Card.vue
+++ src/views/Card.vue
@@ -5,14 +5,14 @@ const { isLoading, cards } = getCards();
 </script>
 
 <template>
-        <div v-if="isLoading">Loading cards...</div>
-        <div v-else class="md:p-6">
-            <div v-for="(card, index) in cards" :key="index">
-                <div>
-                    <p class="md:h-[15rem] md:w-[25rem] md:border md:border-black md:rounded">
-                    {{ card?.name }}{{ card?.description }}
-                    </p>
-                </div>
+    <div v-if="isLoading">Loading cards...</div>
+    <div v-else class="md:p-6">
+        <div v-for="(card, index) in cards" :key="index">
+            <div>
+                <p class="md:h-[15rem] md:w-[25rem] md:border md:border-black md:rounded">
+                {{ card?.name }}{{ card?.description }}
+                </p>
             </div>
         </div>
+    </div>
 </template>
