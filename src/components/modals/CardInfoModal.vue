ade704018851f01c22a1fb3afa45b780077f9e72
jakeNorman007 <jacob.a.norman91@gmail.com>
2024-03-13 23:21:55 -0400

    getting some work done with the cards

--- src/components/modals/CardInfoModal.vue
+++ src/components/modals/CardInfoModal.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import HamburgerIcon from '../../icons/HamburgerIcon.vue';
+import EditCardModal from "../../components/modals/EditCardModal.vue";
 import { ref } from 'vue';
 
 const modalOpen = ref<boolean>(false);
@@ -13,19 +14,14 @@ defineProps(["cardName", "cardDescription", "cardId"],)
         <HamburgerIcon />
     </button>
     <div v-if="modalOpen" class="h-screen fixed bg-black z-[998] w-full left-0 top-0 opacity-25">
-    <Teleport to="body">
+        <Teleport to="body">
         <div v-if="modalOpen" class="gap-3 p-4 flex flex-col bg-white fixed z-[999] w-[35rem] 
             h-[37rem] left-[11%] md:left-[30%] top-[40%] md:top-[15%]">
             <div class="flex justify-between">
                 <p>Your card.</p>
                 <button @click="toggleEditCard = true">
-                    <HamburgerIcon />
+                    <EditCardModal />
                 </button>
-                <div v-if="toggleEditCard" class="bg-gray-200 flex flex-col fixed z-[997] left-[59.3%] 
-                    top-[11.5rem]">
-                    <button class="hover:bg-green-300 px-3 py-2">Edit card</button>
-                    <button @click="toggleEditCard = false" class="hover:bg-green-300 px-3 py-2">Close</button>
-                </div>
             </div>
             <p>Card ID: {{ cardId }}</p>
             <p>Card name: {{ cardName }}</p>
@@ -35,6 +31,6 @@ defineProps(["cardName", "cardDescription", "cardId"],)
                 <p>Close</p>
             </button>
         </div>
-    </Teleport>
+        </Teleport>
     </div>
 </template>
---
+++ src/components/modals/EditCardModal.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import HamburgerIcon from '../../icons/HamburgerIcon.vue';
+import { ref } from 'vue';
+
+const modalOpen = ref<boolean>(false);
+const toggleEditCard = ref<boolean>(false);
+
+defineProps(["cardName", "cardDescription", "cardId"],)
+
+function handleRefresh() {
+    window.location.reload();
+};
+</script>
+
+<template>
+    <button @click="toggleEditCard = true">
+        <HamburgerIcon />
+    </button>
+    <div v-if="toggleEditCard" class="shadow bg-white border border-black flex flex-col absolute z-[997] 
+        left-[79.5%] top-[4.5%]">
+        <button @click="modalOpen = true, toggleEditCard = false" class="shadow-inner hover:bg-green-300 px-3 py-2">
+            <p>Edit card</p>
+        </button>
+        <button @click="toggleEditCard = false" class="shadow-inner border-t border-black hover:bg-green-300 px-3 
+            py-2">
+            <p>Close</p>
+        </button>
+    </div>
+        <Teleport to="body">
+        <div v-if="modalOpen" class="gap-3 p-4 flex flex-col bg-white fixed z-[999] w-[35rem] 
+            h-[37rem] left-[11%] md:left-[30%] top-[40%] md:top-[15%]">
+            <p>F U</p>
+            <button @click="handleRefresh" class="w-full hover:bg-green-300 py-2 bg-green-400">
+                <p>Close</p>
+            </button>
+        </div>
+        </Teleport>
+</template>
