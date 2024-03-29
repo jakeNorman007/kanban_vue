ee9a9887ec102e6a2529da89fc35ac9ca43399ef
jakeNorman007 <jacob.a.norman91@gmail.com>
2024-03-29 16:37:18 -0400

    starting card editing process

--- src/services/cardApi.js
+++ src/services/cardApi.js
@@ -11,3 +11,17 @@ export async function getCards(id) {
 
     return data;
 };
+
+//**UPDATE**//
+//updates/edits the card's name, it know's based off the list id in the edit modal
+export async function updateCard(cardName, id) {
+    console.log(cardName, id);
+    const { data, error } = await supabase.from("card").update({ cardName, id }).eq("id", id).single();
+
+    if (error) {
+        console.log(error);
+        throw new Error("Card name could not be updated");
+    }
+
+    return data;
+};
